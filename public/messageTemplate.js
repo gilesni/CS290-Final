4022d9a92c740fe1863280f78ab45c12ab5c732e
akinde <akind@oregonstate.edu>
2017-06-11 18:47:52 -0700

    Moved stuff around, server is working with 404 page to start with node modules

---
+++ public/messageTemplate.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['message'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "\n";
+},"useData":true});
+})();
--- server.js
+++ server.js
@@ -2,10 +2,20 @@ var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
-var twitData = require('./twitData');
+var twitData = require('./groups.json');
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function (req, res) {
+    res.status(404)
+    res.render('404Page')
+});
+
+var port = process.env.PORT || 3000
+app.listen(port, function () {
+    console.log("Server listening on port ", port);
+});
