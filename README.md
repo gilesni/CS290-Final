d58887298a984a23fc024396b0abbf61d759de58
akinde <akind@oregonstate.edu>
2017-06-11 19:50:03 -0700

    Check README.md

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Final Project for CS290 Spring 2017 @ Oregon State University
 
 Basic Website using Express, handlebars, and node.js. Website allows for users and logins, then joining groups where they can partake in basic text chatting capabilities.
+
+--->I have reconstructed the webpage via handlebars so that tomorrow we can work on the javascript
+- Dean A. Akin<---
--- server.js
+++ server.js
@@ -3,12 +3,20 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var twitData = require('./groups.json');
+var sampleData = require('./sample.json')
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res, next) {
+    var tempArgs = {
+        Epost: sampleData
+    }
+    res.render('mainPage', tempArgs)
+});
 
 app.get('*', function (req, res) {
     res.status(404)
@@ -17,5 +25,5 @@ app.get('*', function (req, res) {
 
 var port = process.env.PORT || 3000
 app.listen(port, function () {
-    console.log("Server listening on port ", port);
+    console.log("Server listening on port", port);
 });
