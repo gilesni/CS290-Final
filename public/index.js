e9992a26328c5b04e3dc472318d3d19431b82afe
Jklihu <gilesni@oregonstate.edu>
2017-06-11 21:17:24 -0700

    add a show and close on the modal

--- public/index.js
+++ public/index.js
@@ -1,2 +1,28 @@
-var account = document.getElementById('login-account-input').value;
-var password = document.getElementById('login-input-element').value;
+function showNameModal() {
+
+    var modalBackdrop = document.getElementById('empty-space');
+    var nameModal = document.getElementById('login-modal');
+
+    // Show the modal and its backdrop.
+    modalBackdrop.classList.remove('hidden');
+    nameModal.classList.remove('hidden');
+
+}
+
+function closeNameModal() {
+
+    var modalBackdrop = document.getElementById('empty-space');
+    var nameModal = document.getElementById('login-modal');
+
+    // Show the modal and its backdrop.
+    modalBackdrop.classList.add('hidden');
+    nameModal.classList.add('hidden');
+
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    var checkInButton = document.getElementById('check-in-button');
+    checkInButton.addEventListener('click', showNameModal);
+
+
+});
